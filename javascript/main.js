5fc291e048ea3cb0fb56917b66be1762aab0ada9
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-13 18:42:09 +0200

    [UPD] Button to start/stop the counter

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,26 @@
 "use strict";
 
-function countdown() {
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+let timer;
+//const myCountdown = setInterval(countdown, 1000);
+start.addEventListener("click", function () {
+  console.log("start timer");
+
+  timer = setInterval(function () {
+    console.log("timer");
+  }, 1000);
+});
+
+stop.addEventListener("click", function () {
+  console.log("stoppo timer");
+  clearInterval(timer);
+});
+
+/*function countdown() {
   for (let i = 10; i >= 0; i--) {
     console.log(i);
   }
   clearInterval(myCountdown);
-}
-
-const myCountdown = setInterval(countdown, 3000);
+  return myCountdown;
+}*/
