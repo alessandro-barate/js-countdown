ea586a93fa75a93bf9eec7d6d63434dacf79f238
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-13 18:10:09 +0200

    [UPD] Logic

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,10 @@
 "use strict";
+
+function countdown() {
+  for (let i = 10; i >= 0; i--) {
+    console.log(i);
+  }
+  clearInterval(myCountdown);
+}
+
+const myCountdown = setInterval(countdown, 3000);
